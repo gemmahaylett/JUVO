722802758c6326db528a7eabfafdb12ff0b05ae0
Gemma Haylett <gemmahaylett@gmail.com>
2014-03-08 17:50:07 -0800

    added effects

--- js/script.js
+++ js/script.js
@@ -28,6 +28,58 @@ $('#charity-list li a').click(function() {
    return false;
 });
 
+$('#hide-comment1').click(function(){
+   var comment_status = $('#hide-comment1').html();
+
+   if(comment_status == 'Hide Comment'){
+      $('#comment-content1').css('background-color', '#e6e6e6');
+      $('#hide-comment1').html('Show Comment');
+   }
+   else{
+      $('#comment-content1').css('background-color', 'none');
+      $('#hide-comment1').html('Hide Comment');
+   }
+});
+
+$('#hide-comment2').click(function(){
+   var comment_status = $('#hide-comment2').html();
+
+   if(comment_status == 'Hide Comment'){
+      $('#comment-content2').css('background-color', '#e6e6e6');
+      $('#hide-comment2').html('Show Comment');
+   }
+   else{
+      $('#comment-content2').css('background-color', 'none');
+      $('#hide-comment2').html('Hide Comment');
+   }
+});
+
+$('#hide-comment3').click(function(){
+   var comment_status = $('#hide-comment3').html();
+
+   if(comment_status == 'Hide Comment'){
+      $('#comment-content3').css('background-color', '#e6e6e6');
+      $('#hide-comment3').html('Show Comment');
+   }
+   else{
+      $('#comment-content3').css('background-color', 'none');
+      $('#hide-comment3').html('Hide Comment');
+   }
+});
+
+$('#hide-comment4').click(function(){
+   var comment_status = $('#hide-comment4').html();
+
+   if(comment_status == 'Hide Comment'){
+      $('#comment-content4').css('background-color', '#e6e6e6');
+      $('#hide-comment4').html('Show Comment');
+   }
+   else{
+      $('#comment-content4').css('background-color', 'none');
+      $('#hide-comment4').html('Hide Comment');
+   }
+});
+
 $(function(){
      
     $('#Grid').mixitup();
